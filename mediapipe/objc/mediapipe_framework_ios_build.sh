809bc18392da9943f3a0a48d6102d7d8a5462322
PeaceByte <65987193+PeaceByte@users.noreply.github.com>
2022-11-20 11:49:51 +0800

    Update mediapipe_framework_ios_build.sh

--- mediapipe/objc/mediapipe_framework_ios_build.sh
+++ mediapipe/objc/mediapipe_framework_ios_build.sh
@@ -6,6 +6,6 @@ xcode-select --install
 brew install bazelisk
 #Set Python 3.7 as the default Python version and install the Python “six” library. This is needed for TensorFlow.
 pip3 install --user six
-pip install numpy
+pip3 install numpy
 #build
 bazel build -c opt --config=ios_arm64 mediapipe/objc:mediapipe_framework_ios
