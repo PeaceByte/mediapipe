1d4c41933bcf3536025bb533a2ceb399878a4728
PeaceByte <65987193+PeaceByte@users.noreply.github.com>
2022-11-20 17:48:36 +0800

    Add files via upload

---
+++ mediapipe/examples/ios/framework/selfiesegmentation/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#Install Xcode, then install the Command Line Tools using:
+xcode-select --install
+#Install Bazelisk .
+brew install bazelisk
+#Set Python 3.7 as the default Python version and install the Python “six” library. This is needed for TensorFlow.
+pip3 install --user six
+pip3 install numpy
+#build
+bazel build -c opt --config=ios_arm64 mediapipe/examples/ios/framework:Selfiesegmentation
