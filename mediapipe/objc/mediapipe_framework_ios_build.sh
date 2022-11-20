9e4afaf48ee21d738236aba54336365b49614dd0
PeaceByte <65987193+PeaceByte@users.noreply.github.com>
2022-11-20 11:32:35 +0800

    Delete mediapipe_framework_ios_build.sh

--- mediapipe/objc/mediapipe_framework_ios_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#Install Xcode, then install the Command Line Tools using:
-xcode-select --install
-#Install Bazelisk .
-brew install bazelisk
-#Set Python 3.7 as the default Python version and install the Python “six” library. This is needed for TensorFlow.
-pip3 install --user six
-#build
-bazel build -c opt --config=ios_arm64 mediapipe/objc/objc:mediapipe_framework_ios
